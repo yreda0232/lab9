556877c41ded170b576464e1183e6d3419557a16
Ramez-Antoine <ramez.antoine1105@gmail.com>
2025-12-01 03:33:46 +0200

    changed

--- src/lab9/ColChecker.java
+++ src/lab9/ColChecker.java
@@ -1,5 +1,11 @@
 package lab9;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ColChecker implements  Checker
 {
     private final int[][] board;
